fe9b0f303d7cb5c3b5ed8a65978eacc15aea8935
abdelrhmanwageh33 <abdelrhmanwageh413@gmail.com>
2026-07-31 14:20:43 +0300

    update index

--- src/index.ts
+++ src/index.ts
@@ -21,5 +21,5 @@ await connectDB();
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(` Server running on http://localhost:${PORT}`);
 });
